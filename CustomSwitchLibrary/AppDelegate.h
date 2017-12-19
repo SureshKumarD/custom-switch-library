aa5ab8ef64eb90a28b1bc74fbc94f5e1581a3498
Suresh Kumar <suresh.kumar@lenskart.in>
2017-12-19 18:57:34 +0530

    Project initiated.

---
+++ CustomSwitchLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomSwitchLibrary
+//
+//  Created by Suresh Kumar on 19/12/17.
+//  Copyright © 2017 Suresh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomSwitchLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomSwitchLibrary
+//
+//  Created by Suresh Kumar on 19/12/17.
+//  Copyright © 2017 Suresh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
